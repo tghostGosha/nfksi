6e2dd6c7ca311018b79e0ef26093f3dd7171ed7a
tghostGosha <tghost@mail.ru>
2022-11-30 13:36:03 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -108,13 +108,14 @@ function swiperCard() {
     if (!init) {
       init = true;
       swiper = new Swiper(".swiper", {
-        modules: [Pagination],
+        modules: [Navigation],
         slidesPerView: "auto",
         centeredSlides: true,
         spaceBetween: 16,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
+
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
       });
     }
